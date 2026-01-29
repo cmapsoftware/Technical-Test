742a6b55c6dc75215693960fcd0aaf58ba8970ec
wizzard262 <jones.steve@live.com>
2026-01-29 13:34:44 +0000

    add ViewModels folder level

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TimesheetTest.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace TimesheetTest.Models.ViewModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
