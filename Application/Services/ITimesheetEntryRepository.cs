d1715cca1376d0b5f66de31bf0df6eb2db83a3cc
wizzard262 <jones.steve@live.com>
2026-01-29 13:40:55 +0000

    add TimesheetEntry domaion model and basic Repository service and interace (Note: some // TODO: comments indicate incomplete functionality)

---
+++ Application/Services/ITimesheetEntryRepository.cs
@@ -0,0 +1,12 @@
+﻿using TimesheetTest.Domain;
+
+namespace TimesheetTest.Application.Services;
+
+public interface ITimesheetEntryRepository
+{
+    IEnumerable<TimesheetEntry> GetAll();
+    TimesheetEntry? Get(int id);
+    void Add(TimesheetEntry entry);
+    void Update(TimesheetEntry entry);
+    void Delete(int id);
+}
---
+++ Domain/TimesheetEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace TimesheetTest.Domain;
+
+// Represents a single row in a user's timesheet. 
+// For test store UserId and ProjectId directly to keep domain simple.
+// (User and Project objects not modeled in this test domain.)
+public class TimesheetEntry
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int ProjectId { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Hours { get; set; }
+    public string? Description { get; set; }
+}
